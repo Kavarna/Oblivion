7da7d1fa4c5938bb4122b976903024da06978c19
Dobrica Stefan <sdobrica188@gmail.com>
2018-07-04 19:17:21 +0300

    Revert "Added btMatrix4x4;"
    This reverts commit 642bbff6f1ae5c9bd30531868090d1252e08658c.

--- Engine/Bullet Physics/Bullet Physics/btBulletCollisionCommon.h
+++ Engine/Bullet Physics/Bullet Physics/btBulletCollisionCommon.h
@@ -62,7 +62,6 @@ subject to the following restrictions:
 #include "LinearMath/btQuickprof.h"
 #include "LinearMath/btIDebugDraw.h"
 #include "LinearMath/btSerializer.h"
-#include "LinearMath/btMatrix4x4.h"
 
 
 #endif //BULLET_COLLISION_COMMON_H
--- Engine/Engine/src/common/commonmath.h
+++ Engine/Engine/src/common/commonmath.h
@@ -1,30 +1,33 @@
 #pragma once
 
-//#define BT_NO_SIMD_OPERATOR_OVERLOADS
-#include <btBulletDynamicsCommon.h>
-#include <btBulletCollisionCommon.h>
+#include <DirectXMath.h>
 
 namespace Math
 {
-
-	inline bool isPointInTriangle(btVector3 const& A,
-		btVector3 const& B, btVector3 const& C, btVector3 const& P)
+	inline bool isPointInTriangle(DirectX::XMFLOAT3 const& V1,
+		DirectX::XMFLOAT3 const& V2, DirectX::XMFLOAT3 const& V3,
+		DirectX::XMFLOAT3 const& Point)
 	{
-		btVector3 v0, v1, v2;
-		v0 = C - A;
-		v1 = B - A;
-		v2 = P - A;
+		using namespace DirectX;
+		XMVECTOR A = XMLoadFloat3(&V1);
+		XMVECTOR B = XMLoadFloat3(&V2);
+		XMVECTOR C = XMLoadFloat3(&V3);
+		XMVECTOR P = XMLoadFloat3(&Point);
 
-		float dot00 = v0.dot(v0);
-		float dot01 = v0.dot(v1);
-		float dot02 = v0.dot(v2);
-		float dot11 = v1.dot(v1);
-		float dot12 = v1.dot(v2);
+		XMVECTOR v0 = C - A;
+		XMVECTOR v1 = B - A;
+		XMVECTOR v2 = P - A;
 
-		float invDenom = 1.0f / (dot00 * dot11 - dot01 * dot01);
+		float dot00 = XMVectorGetX(XMVector3Dot(v0, v0));
+		float dot01 = XMVectorGetX(XMVector3Dot(v0, v1));
+		float dot02 = XMVectorGetX(XMVector3Dot(v0, v2));
+		float dot11 = XMVectorGetX(XMVector3Dot(v1, v1));
+		float dot12 = XMVectorGetX(XMVector3Dot(v1, v2));
+
+		float invDenom = 1.0f / ( dot00 * dot11 - dot01 * dot01 );
 		float u, v;
-		u = (dot11 * dot02 - dot01 * dot12) * invDenom;
-		v = (dot00 * dot12 - dot01 * dot02) * invDenom;
+		u = ( dot11 * dot02 - dot01 * dot12 ) * invDenom;
+		v = ( dot00 * dot12 - dot01 * dot02 ) * invDenom;
 
 		return u >= 0 && v >= 0 && u + v < 1;
 	}
